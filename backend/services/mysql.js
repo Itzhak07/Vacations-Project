7bd488459bb64092c890e7b52fb56cf8dfbfbe59
Itzhak07 <itzhak07@gmail.com>
2020-02-16 22:30:14 +0200

    minor fixes

--- backend/services/mysql.js
+++ backend/services/mysql.js
@@ -3,7 +3,7 @@ var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
   port: "3306",
-  password: "Itzhak07",
+  password: "Enter Your Password",
   database: "vacations_db",
   dateStrings: "date"
 });
